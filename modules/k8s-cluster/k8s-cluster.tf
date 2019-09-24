71feb417936a3778f3f5a3dc2c3d3abe505fa66a
Sergii Smetanin <s.smetanin@hubber.pro>
2019-09-24 15:20:13 +0300

    add cluster

---
+++ modules/k8s-cluster/k8s-cluster.tf
@@ -0,0 +1,12 @@
+resource "google_container_cluster" "k8s-cluster" {
+  name                = "k8s-${var.project}-${var.env}"
+  location            = var.location
+
+  initial_node_count  = 1
+
+  addons_config {
+    kubernetes_dashboard {
+      disabled = ! var.dashboard
+    }
+  }
+}
