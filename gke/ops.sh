f244aa1cd07cc368051e4974dfb6838a2d444bb9
Sergii Smetanin <s.smetanin@hubber.pro>
2019-10-29 14:16:17 +0200

    update backend

---
+++ gke/ops.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ops=$1
+env=$2
+
+date=$(date '+%Y%m%d%H%M');
+
+rootpath="./.terraform/${env}"
+logpath="${rootpath}/logs"
+varpath="${rootpath}/vars"
+planpath="${rootpath}/plans"
+#statepath="${rootpath}/state"
+
+mkdir -p ${logpath} ${varpath} ${planpath} #${statepath}
+
+#cp terraform.tfstate ${statepath}/${date}.tfstate
+
+case ${ops} in
+
+    "apply" )
+
+        terraform validate 
+        terraform plan -var-file ${varpath}/variables.tfvars -out ${planpath}/${date}.tfplan #-state ${statepath}/terraform.tfstate
+        terraform apply ${planpath}/${date}.tfplan
+        
+        if [ -f "${planpath}/${date}.tfplan" ]; then
+            echo "Tfplan was successfully generated at ${date} by $USER" >> ${logpath}/ops.log
+        fi
+
+        ;;
+
+    "destroy" )
+
+        terraform destroy -var-file ${varpath}/variables.tfvars
+
+        echo "Role was successfully destroy at ${date} by $USER" >> ${logpath}/ops.log
+        ;;
+
+    *) echo >&2 "Invalid option: $@"; 
+        exit 1;;
+esac
